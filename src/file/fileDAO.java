7b12c6095bdc587a39d86482dc1490067436ac32
hyebinppark <gpqls304@naver.com>
2019-12-03 18:20:46 +0900

    filedao

--- src/file/fileDAO.java
+++ src/file/fileDAO.java
@@ -12,13 +12,15 @@ public class fileDAO {
 
 		try {
 
-			String dbURL = "jdbc:mysql://localhost:3306/file?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&characterEncoding=UTF-8";
-			String dbID = "root";
-			String dbPW = "dlsduqdl";
-
+			String url = "jdbc:mysql://10.10.5.15:3306/op_242b1bc8_a6d7_417e_8fcd_77b2d623d514?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+			String id = "1afe79199eb792d8";
+			String pw = "c32ca248cd0ba4a7";
+			
+//			String url = "jdbc:mysql://localhost:3306/file?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&characterEncoding=UTF-8";
+//			String id = "root";
+//			String pw = "";
 			Class.forName("com.mysql.cj.jdbc.Driver");
-
-			conn = DriverManager.getConnection(dbURL, dbID, dbPW);
+			conn = DriverManager.getConnection(url, id, pw);
 
 		} catch(Exception e) {
 
