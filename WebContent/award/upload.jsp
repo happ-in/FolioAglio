b90269352b67bf9342ae0793a099b0fd0a9a821d
najio <najio@DESKTOP-9BS7GCP>
2019-12-06 01:48:07 +0900

    add_upload

---
+++ WebContent/award/upload.jsp
@@ -0,0 +1,45 @@
+<%@ page import="file.fileDAO" %>
+<%@ page import="java.io.File" %>
+<%@page import="java.util.Enumeration"%>
+<%@ page import="com.oreilly.servlet.multipart.DefaultFileRenamePolicy" %>
+<%@ page import="com.oreilly.servlet.MultipartRequest" %>
+<%@ page language="java" contentType="text/html; charset=utf-8"
+    pageEncoding="utf-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Insert title here</title>
+</head>
+<body>
+   <%
+   	  String directory = application.getRealPath("award/IMG"); //절대경로를 가져옴
+   	  
+      int maxSize = 1024 * 1024 * 100;
+      String encoding = "UTF-8";
+      
+      File f = new File(directory);
+      if(!f.isFile()){
+    	  if(!f.isDirectory()){
+    		  f.mkdir();
+    	  }
+      }
+      
+      MultipartRequest multipartRequest
+      = new MultipartRequest(request, directory, maxSize, encoding);
+      
+      
+      String fileName = multipartRequest.getOriginalFileName("picture");
+      String fileRealName = multipartRequest.getFilesystemName("picture");
+      
+      Object session_object=session.getAttribute("signedUser");
+      String session_name=(String)session_object;
+      
+      String com = new String(request.getParameter("com").getBytes("ISO-8859-1"), "UTF-8");
+      
+      new fileDAO().upload("award", "IMG/"+fileRealName, session_name, com);
+   %>
+   
+   
+</body>
+</html>
--- src/file/fileDAO.java
+++ src/file/fileDAO.java
@@ -4,76 +4,91 @@ import java.sql.*;
 
 public class fileDAO {
 
-	private Connection conn;
+   private Connection conn;
 
-	// 생성자를 통해 db연결 해줌
+   // 생성자를 통해 db연결 해줌
 
-	public fileDAO() {
+   public fileDAO() {
 
-		try {
+      try {
 
-			String url = "jdbc:mysql://10.10.5.15:3306/op_242b1bc8_a6d7_417e_8fcd_77b2d623d514?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-			String id = "7d34c38ba9686d5a";
-			String pw = "c32ca248cd0ba4a7";
-			
-//			String url = "jdbc:mysql://localhost:3306/file?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&characterEncoding=UTF-8";
-//			String id = "root";
-//			String pw = "system";
+         String url = "jdbc:mysql://10.10.5.15:3306/op_242b1bc8_a6d7_417e_8fcd_77b2d623d514?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+         String id = "7d34c38ba9686d5a";
+         String pw = "c32ca248cd0ba4a7";
+         
+//         String url = "jdbc:mysql://localhost:3306/file?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&characterEncoding=UTF-8";
+//         String id = "root";
+//         String pw = "system";
 
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection(url, id, pw);
+         Class.forName("com.mysql.cj.jdbc.Driver");
+         conn = DriverManager.getConnection(url, id, pw);
 
-		} catch(Exception e) {
+      } catch(Exception e) {
 
-			e.printStackTrace();
+         e.printStackTrace();
 
-		}
+      }
 
-	}
+   }
 
 
-	public int upload(String table, String fileRealName, String session_name, String com) {
-		String SQL = "";
-		
-		if(table.equals("carrier")) {	
-			SQL = "update carrier set picture=? where id=? and company_name=?";
-		}
-		try {
+   public int upload(String table, String fileRealName, String session_name, String com) {
+      String SQL = "";
+      
+      if(table.equals("abroad")) {
+         SQL = "update abroad set picture=? where id=? and country_name=?";
+      }
 
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
+      else if(table.equals("award")) {
+         SQL = "update award set picture=? where id=? and awd_name=?";
+      }
 
-			//pstmt.setString(1,  table);
+      else if(table.equals("carrier")) {   
+         SQL = "update carrier set picture=? where id=? and company_name=?";
+      }
 
-			pstmt.setString(1,  fileRealName);
-			
-			pstmt.setString(2,  session_name);
-			
-			pstmt.setString(3, com);
 
-			return pstmt.executeUpdate();
+      else if(table.equals("external_activities")) {
+         SQL = "update external_activities set picture=? where id=? and group_name=?";
+      }
 
-		} catch(Exception e) {
+      try {
 
-			e.printStackTrace();
+         PreparedStatement pstmt = conn.prepareStatement(SQL);
 
-		}
+         //pstmt.setString(1,  table);
 
-		return -1;
+         pstmt.setString(1,  fileRealName);
+         
+         pstmt.setString(2,  session_name);
+         
+         pstmt.setString(3, com);
 
-	}
+         return pstmt.executeUpdate();
 
-	
-	public int p_upload(String table, String fileRealName, String session_name) {
-		String SQL = "update personal_information set picture=? where id=?";
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(SQL);
-			pstmt.setString(1,  fileRealName);
-			pstmt.setString(2,  session_name);
-			return pstmt.executeUpdate();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return -1;
-	}
+      } catch(Exception e) {
+
+         e.printStackTrace();
+
+      }
+      
+
+      return -1;
+
+   }
+
+   
+   public int p_upload(String table, String fileRealName, String session_name) {
+      String SQL = "update personal_information set picture=? where id=?";
+      try {
+         PreparedStatement pstmt = conn.prepareStatement(SQL);
+         pstmt.setString(1,  fileRealName);
+         pstmt.setString(2,  session_name);
+         return pstmt.executeUpdate();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      return -1;
+   }
 
 }
