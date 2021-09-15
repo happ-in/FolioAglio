834bf5c6f3bc34501b5677669c4db422b697a31b
Hyebin Park <36289638+happ-in@users.noreply.github.com>
2021-09-15 23:25:52 +0900

    Update DBUtil.java

--- src/file/DBUtil.java
+++ src/file/DBUtil.java
@@ -14,9 +14,6 @@ public class DBUtil {
 			String id = "b5ccb48763e31451";
 			String pw = "c32ca248cd0ba4a7";
 			
-//			String url = "jdbc:mysql://localhost:3306/file?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC&characterEncoding=UTF-8";
-//			String id = "root";
-//			String pw = "";
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			conn = DriverManager.getConnection(url, id, pw);
 		} catch(Exception e) {
